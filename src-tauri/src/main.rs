41494adbe8d5f551cce2375dace0a8c566eaf6d7
JinSo <kimjinso@qq.com>
2023-08-24 14:49:20 +0800

    feat( shortcut ): ✨ temp

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+  // .setup(|app| {
+  //   let window = app.get_window("main").unwrap();
+  //   window.listen("tauri://event/keyboard/down", move | event | {
+  //       let window = app.get_window("main").unwrap();
+  //       let input = event.payload();
+  //       let now = std::time::Instant::now();
+
+  //       启动一个计时器
+  //       std::thread::spawn(move || {
+  //         let duration = now.elapsed();
+
+  //         500毫秒后触发长按事件
+  //         if duration >= std::time::Duration::from_millis(500) {
+  //           window.emit("longPress", input).unwrap();
+  //         }
+  //       });
+  //     });
+
+  //   Ok(())
+  // })
+    .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+  format!("Hello, {}! You've been greeted from Rust!", name)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+'use client'
+
+import { initShortCut } from '@/lib/utils/shortcut'
+import { invoke } from '@tauri-apps/api'
+import { useEffect } from 'react'
+
 export default function Home() {
-  return <div>CheatSheet</div>
+  useEffect(() => {
+    initShortCut()
+
+    invoke<string>('greet', { name: 'JinSo' }).then(console.log)
+  }, [])
+
+  return (
+    <div>
+      CheatSheet
+      <div className='bg-white w-10 h-10'>123</div>
+    </div>
+  )
 }
