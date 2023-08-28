163e6fb87be3e493b5b11575cfdde00cc0b4b718
JinSo <kimjinso@qq.com>
2023-08-28 20:14:07 +0800

    fix( shortcut ): 🐛 window is not define

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { invoke } from '@tauri-apps/api'
 import { listen } from '@tauri-apps/api/event'
 import { useEffect, useState } from 'react'
 
@@ -12,6 +11,9 @@ export default function Home() {
       console.log('payload', event.payload)
       setActiveAppName(event.payload as string)
     })
+
+    // 异步引入，确保处于浏览器环境
+    const { invoke } = await import('@tauri-apps/api')
     // 先通知后端已经监听，再去初始化全局热键（避免监听被堵塞）
     invoke('init_listen')
   }
