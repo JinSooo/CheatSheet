8b9b3f7cb82f8dd1b2e2ed92562a0c625df8ee97
JinSo <kimjinso@qq.com>
2023-08-30 14:17:48 +0800

    fix( shortcut ): 🐛 retrieve tauri globalshortcut

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,52 +2,56 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use active_win_pos_rs::get_active_window;
-use global_hotkey::{
-    hotkey::{Code, HotKey},
-    GlobalHotKeyEvent, GlobalHotKeyManager,
-};
-use tauri::Manager;
+use tauri::{GlobalShortcutManager, GlobalWindowEvent, Manager};
+
+static GLOBAL_HOTKEY_SHORTCUT: &str = "F2";
 
 fn main() {
     tauri::Builder::default()
         // 失去焦点自动隐藏
-        .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::Focused(focused) => {
-                if !focused {
-                    event.window().hide().unwrap();
-                }
-            }
-            _ => {}
-        })
+        .on_window_event(|event| init_tauri_event(event))
         .setup(|app| {
-            let main_window = app.get_window("main").unwrap();
-            init_hotkey(main_window);
+            init_hotkey(app.app_handle().clone());
             Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
-// 初始化全局热键
-fn init_hotkey(window: tauri::Window) {
-    // 热键
-    let hotkey_manager = GlobalHotKeyManager::new().unwrap();
-    let hotkey = HotKey::new(None, Code::F2);
-    hotkey_manager.register(hotkey).unwrap();
-
-    GlobalHotKeyEvent::set_event_handler(Some(move |e: GlobalHotKeyEvent| {
-        if e.id == hotkey.id() {
-            if window.is_visible().unwrap() {
-                window.hide().unwrap();
-            } else {
-                let active_app_name = get_current_active_window();
-                window.emit("active-window", active_app_name).unwrap();
-                // 等100ms再显示，留时间给页面进行渲染
-                std::thread::sleep(std::time::Duration::from_millis(100));
-                window.show().unwrap();
+// 初始化Tauri事件
+fn init_tauri_event(e: GlobalWindowEvent) {
+    // 失去焦点自动隐藏
+    match e.event() {
+        tauri::WindowEvent::Focused(focused) => {
+            if !focused {
+                e.window().hide().unwrap();
             }
         }
-    }))
+        _ => (),
+    }
+}
+
+// 初始化全局热键
+fn init_hotkey(app: tauri::AppHandle) {
+    let main_window = app.get_window("main").unwrap();
+    app.global_shortcut_manager()
+        .register(GLOBAL_HOTKEY_SHORTCUT, move || {
+            hotkey_handler(main_window.clone());
+        })
+        .unwrap();
+}
+
+// 全局热键处理
+fn hotkey_handler(window: tauri::Window) {
+    if window.is_visible().unwrap() {
+        window.hide().unwrap();
+    } else {
+        let active_app_name = get_current_active_window();
+        window.emit("active-window", active_app_name).unwrap();
+        // 等100ms再显示，留时间给页面进行渲染
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        window.show().unwrap();
+    }
 }
 
 // 获取当前聚焦程序
