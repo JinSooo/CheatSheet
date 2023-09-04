4c2194bc311acdff02184a03bb02dbb5786b303f
JinSo <kimjinso@qq.com>
2023-09-04 21:02:22 +0800

    feat( config ): ✨ beauitify

--- src/components/Config/ConfigAside.tsx
+++ src/components/Config/ConfigAside.tsx
@@ -2,17 +2,27 @@
 
 import useTheme from '@/lib/hooks/useTheme'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import routes from './routes'
 
 const ConfigAside = () => {
   // 为了初始化主题配色
   const _ = useTheme()
+  const pathname = usePathname()
+  console.log(pathname)
 
   return (
-    <ul className='menu w-44 rounded-box h-full gap-2'>
+    <ul className='menu w-44 h-full gap-2 pt-2'>
       {routes.map((route) => (
         <li key={route.path}>
-          <Link href={route.path}>{route.name}</Link>
+          <Link
+            className={pathname === route.path ? 'active bg-[var(--background)]!' : ''}
+            href={route.path}
+            // TODO: 目前Light模式下色彩不匹配
+            // style={{ backgroundColor: pathname === route.path ? 'var(--background)' : '' }}
+          >
+            {route.name}
+          </Link>
         </li>
       ))}
     </ul>
