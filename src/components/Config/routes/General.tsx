86da88cc8981db549458ef23a26d1800db4e4a02
JinSo <kimjinso@qq.com>
2023-09-06 23:54:54 +0800

    feat( config ): ✨ window alwaysOnTop & size

--- src/components/Config/routes/General.tsx
+++ src/components/Config/routes/General.tsx
@@ -2,7 +2,8 @@
 
 import useTheme from '@/lib/hooks/useTheme'
 import { emit } from '@tauri-apps/api/event'
-import { ChangeEvent } from 'react'
+import { LogicalSize, Monitor, WebviewWindow } from '@tauri-apps/api/window'
+import { ChangeEvent, useEffect, useRef } from 'react'
 import Checkbox from '../common/Checkbox'
 import { Container } from '../common/Container'
 import Range from '../common/Range'
@@ -10,18 +11,45 @@ import Select from '../common/Select'
 
 const General = () => {
   const { setTheme } = useTheme()
+  const mainWindow = useRef<WebviewWindow | null>()
+  const monitor = useRef<Monitor | null>()
 
+  const init = async () => {
+    const { getAll, currentMonitor } = await import('@tauri-apps/api/window')
+    // 获取CheatSheet窗口
+    mainWindow.current = getAll().find((window) => window.label === 'main')
+    // 获取当前显示器信息
+    monitor.current = await currentMonitor()
+  }
+
+  // 主题
   const handleThemeChange = (e: ChangeEvent<HTMLSelectElement>) => {
     setTheme(e.target.value)
   }
-
+  // 窗口透明度
   const handleWindowOpacity = (e: ChangeEvent<HTMLInputElement>) => {
     emit('background_opacity', +e.target.value / 10)
   }
-
+  // 窗口圆角
   const handleWindowBorderRadius = (e: ChangeEvent<HTMLInputElement>) => {
     emit('window_border_radius', +e.target.value)
   }
+  // 窗口置顶
+  const handleWindowAlwaysOnTop = (e: ChangeEvent<HTMLInputElement>) => {
+    mainWindow.current?.setAlwaysOnTop(e.target.checked)
+  }
+  // 窗口大小
+  const handleWindowSize = async (e: ChangeEvent<HTMLInputElement>) => {
+    const ratio = +e.target.value / 100
+    mainWindow.current?.setSize(
+      new LogicalSize((monitor.current?.size.width ?? 1920) * ratio, (monitor.current?.size.height ?? 1080) * ratio),
+    )
+    mainWindow.current?.center()
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
 
   return (
     <Container title='通用'>
@@ -36,7 +64,7 @@ const General = () => {
         </li>
         <li>
           <p>窗口置顶</p>
-          <Checkbox />
+          <Checkbox onChange={handleWindowAlwaysOnTop} />
         </li>
         <li>
           <p>窗口透明度</p>
@@ -44,7 +72,7 @@ const General = () => {
         </li>
         <li>
           <p>窗口大小百分比</p>
-          <Range min={0} max={100} />
+          <Range min={50} max={100} onChange={handleWindowSize} />
         </li>
         <li>
           <p>窗口圆角大小</p>
