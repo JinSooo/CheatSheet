75f27693768c4038afc4c56e350702636e2acc23
JinSo <kimjinso@qq.com>
2023-09-08 15:53:17 +0800

    fix( config/hotkey ): 🐛 keyboard submit

--- src/components/Config/common/Keyboard.tsx
+++ src/components/Config/common/Keyboard.tsx
@@ -14,12 +14,7 @@ const Keyboard = ({ submit, tooltip, command, ...props }: Props) => {
       <div className='w-full absolute top-[50%] translate-y-[-50%] left-2 pointer-events-none'>
         <ShortCutCommand command={command ?? ''} gap={1} />
       </div>
-      {/* TODO: 按钮点击向下偏移，没找到问题所在，可以当作一种特色（⊙ｏ⊙） */}
-      <button
-        className='btn w-8 h-8 min-h-8 btn-ghost absolute top-[50%] translate-y-[-50%] right-0'
-        type='button'
-        onClick={submit}
-      >
+      <button className='btn w-8 h-8 min-h-8 btn-ghost absolute top-0 right-0' type='button' onClick={submit}>
         √
       </button>
     </div>
