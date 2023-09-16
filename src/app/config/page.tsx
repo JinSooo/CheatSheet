81c2a1529615199cbd7aafd2ff7d720da104f773
JinSo <kimjinso@qq.com>
2023-09-16 11:15:40 +0800

    fix: 🐛 bundle config show router “/”

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from 'next/navigation'
+import General from '@/components/Config/routes/General'
 
 export default function Page() {
-  redirect('/config/general')
-  return <></>
+  // TODO: 这边默认显示General页面，但是多次点击General页面的话会导致重复渲染
+  return <General />
 }
