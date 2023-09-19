f35512de7d780ca8024af2dc55ba069a10a677fa
JinSo <kimjinso@qq.com>
2023-09-20 00:06:28 +0800

    feat( shortcut ): ✨ shortcut command style

--- src/components/common/ShortCutCommand.tsx
+++ src/components/common/ShortCutCommand.tsx
@@ -12,34 +12,43 @@ interface Props {
 // 根据不同类型的快捷键生成对应的快捷键
 const generateCommand = (type: ShortCutKind['type'], arr: ShortCutKind['arr'], command: string) => {
   if (type === 'combination') {
-    return arr.map((key, i) => (
-      <>
-        {key.map((item, index) => (
-          <div
-            key={command + (typeof item === 'string' ? item : item.key?.toString()) + index.toString()}
-            className='kbd kbd-sm'
-          >
-            {item}
-          </div>
-        ))}
-        {i !== arr.length - 1 && <div className='px-2'>&</div>}
-      </>
-    ))
+    return (
+      <div className='flex justify-center items-center'>
+        <div className='font-bold pr-1 text-sm'>&</div>
+        <div className='flex flex-col gap-1'>
+          {arr.map((key, index) => (
+            <div key={command + index.toString()}>
+              {key.map((item, index) => (
+                <div
+                  key={command + (typeof item === 'string' ? item : item.key?.toString()) + index.toString()}
+                  className='kbd kbd-sm'
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    )
   } else if (type === 'multi') {
     return (
-      <div className='flex flex-col gap-1'>
-        {arr.map((key, index) => (
-          <div key={command + index.toString()}>
-            {key.map((item, index) => (
-              <div
-                key={command + (typeof item === 'string' ? item : item.key?.toString()) + index.toString()}
-                className='kbd kbd-sm'
-              >
-                {item}
-              </div>
-            ))}
-          </div>
-        ))}
+      <div className='flex justify-center items-center'>
+        <div className='font-bold pr-1 text-sm'>|</div>
+        <div className='flex flex-col gap-1'>
+          {arr.map((key, index) => (
+            <div key={command + index.toString()}>
+              {key.map((item, index) => (
+                <div
+                  key={command + (typeof item === 'string' ? item : item.key?.toString()) + index.toString()}
+                  className='kbd kbd-sm'
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     )
   } else if (type === 'normal') {
@@ -64,7 +73,7 @@ export const ShortCutCommand = ({ command, gap = 0, direction = 'end' }: Props) 
   return (
     <div
       className={`flex flex-wrap ${gap > 0 ? `gap-${gap}` : ''} ${
-        direction === 'start' ? 'justify-start' : 'justify-end'
+        direction === 'start' ? 'justify-start' : 'justify-start'
       }`}
     >
       {commandEle}
