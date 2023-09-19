d0d89671bdcd696ac3e6c78e9f49f7568c9833de
JinSo <kimjinso@qq.com>
2023-09-19 17:31:21 +0800

    feat( update ): ✨ check update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import { getOSType } from '@/lib/utils'
 import { useEffect, useState } from 'react'
 import { Store } from 'tauri-plugin-store-api'
 import { initConfigStore } from '@/lib/utils/store'
+import { listen } from '@tauri-apps/api/event'
+import { checkAppUpdate } from '@/lib/utils/updater'
 
 export default function RootLayout({
   children,
@@ -29,9 +31,16 @@ export default function RootLayout({
     setConfigStore(await initConfigStore())
   }
 
+  const initListen = async () => {
+    await listen('check_update', async () => {
+      await checkAppUpdate()
+    })
+  }
+
   useEffect(() => {
     initOS()
     initConfig()
+    initListen()
   }, [])
 
   return (
